7b1215d13fdf10d0f52f8483eb40cd87583d7916
Jeremi-Koloma <kolomajeremi60@gmail.com>
2025-06-02 22:59:19 +0000

    Ajout une méthode de produit dans la classe calculatrice

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,13 @@ import tech.sahelcrea.services.Calculatrice;
 public class Main {
     public static void main(String[] args) {
 
-    // Créons une instance de notre classe calculatrice
+        // Créons une instance de notre classe Calculatrice
         Calculatrice calculatrice = new Calculatrice();
 
         int resultatAddition = calculatrice.addition(10, 5);
         System.out.println(resultatAddition);
+
+        int resultatProduit = calculatrice.produit(9, 10);
+        System.out.println(resultatProduit);
     }
 }
--- src/tech/sahelcrea/services/Calculatrice.java
+++ src/tech/sahelcrea/services/Calculatrice.java
@@ -1,9 +1,13 @@
 package tech.sahelcrea.services;
 
 public class Calculatrice {
-    // Une méthode pour additionner deux nombre
+    // Une méthode pour additionner deux nombres
     public int addition(int premierEntier, int secondEntier) {
-        int resultat = premierEntier + secondEntier;
-        return resultat;
+        return premierEntier + secondEntier;
+    }
+
+    // Une méthode pour faire le produit de deux nombres
+    public int produit(int premierEntier, int secondEntier) {
+        return premierEntier * secondEntier;
     }
 }
