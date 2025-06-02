2f058abc761d90b5ac4750110d71b3370142c461
Jeremi-Koloma <kolomajeremi60@gmail.com>
2025-06-02 22:44:56 +0000

    Creation d'un classe

---
+++ src/tech/sahelcrea/services/Calculatrice.java
@@ -0,0 +1,9 @@
+package tech.sahelcrea.services;
+
+public class Calculatrice {
+    // Une méthode pour additionner deux nombre
+    public int addition(int premierEntier, int secondEntier) {
+        int resultat = premierEntier + secondEntier;
+        return resultat;
+    }
+}
