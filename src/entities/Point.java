c526e2077301e337a6a883907e290964daa33950
Jeremi-Koloma <kolomajeremi60@gmail.com>
2025-06-04 13:08:05 +0000

    Ajout d'encapstulation, accesseurs, de l'entite Point

--- src/entities/Point.java
+++ src/entities/Point.java
@@ -1,4 +1,36 @@
 package entities;
 
 public class Point {
+    // Encaptulation
+    private int x;
+    private int y;
+
+    // Constructeur sans args, pour la création de l'object Point avec les valeurs par défaut
+    public Point() {
+    }
+
+    // Constructeur avec args, pour la création de l'object Point avec des valeurs précises
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
+    // accesseurs
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
